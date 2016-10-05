5e7782de4300059b57a2e6b631cb6573b86ada7f
Clive Yip <yipsifu@gmail.com>
2016-10-05 22:08:16 +0000

    remove key pairs with zero value

--- character-counting.js
+++ character-counting.js
@@ -5,7 +5,6 @@ function countLetters(myString){
   // create a-z Object - key: a-z, value: 0
   aZObject = {a: 0, b: 0, c: 0, d: 0, e: 0, f: 0, g: 0, h: 0, i: 0, j: 0, k: 0, l: 0, m: 0, n: 0, o: 0, p: 0, q: 0, r: 0, s: 0, t: 0, u: 0, v: 0, w: 0, x: 0, y: 0, z: 0}
   // each a-z key compare with each char in myString
-  // value +1 if math
   for (var i in aZObject) {
     for (var j in noSpace) {
       if (i == noSpace[j]) {
@@ -14,7 +13,15 @@ function countLetters(myString){
     }
   }
 
-  return aZObject;
+  // return only keys with value > 0
+  var aZObjectNoZeros = {};
+  for (var i in aZObject) {
+    if (aZObject[i] > 0) {
+      aZObjectNoZeros[i] = aZObject[i];
+    }
+  }
+
+  return aZObjectNoZeros;
 }
 
-console.log(countLetters('lighthouse'));
+console.log(countLetters('lighthouse in the house'));
